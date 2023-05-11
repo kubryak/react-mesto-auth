2121fbac3f3d956b6808f06d0abbda577577b9ec
kubryak <al.lcn@yandex.ru>
2023-05-12 03:07:13 +0500

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='react-mesto'>
+  <BrowserRouter basename='react-mesto-auth'>
     <App />
   </BrowserRouter>
 );
